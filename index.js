b3ba9f6cdf65428458e7781d86c94d2ed6fe6b6d
fov42550564 <42550564@qq.com>
2016-10-17 17:05:06 +0800

    发布1.0.0

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-/*
-* (The MIT License)
-* Copyright (c) 2015-2016 YunJiang.Fang <42550564@qq.com>
-* @providesModule battery-status
-* @flow-weak
-*/
 'use strict';
 
-var React = require('react-native');
-var {
-    View,
-    Text,
-} = React;
+var Battery = require('./libs/battery.js');
 
-module.exports = React.createClass({
-    render() {
-        return (
-            <View style={{flex:1, justifyContent: 'center', alignItems: 'center',}}>
-                <Text>
-                    come soon
-                </Text>
-            </View>
-        )
-    }
-});
+module.exports = Battery;
